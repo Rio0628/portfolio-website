9cb279b1b85d16870b8269c423de75170b74477e
Rio0628 <mariomdomenech@gmail.com>
2021-12-16 23:27:17 -0500

    Fixed Small Error
    - Fixed small error with the past commit where the about view's chart element where deleted.

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -16,7 +16,50 @@ const AboutView = () => {
     
             </p>
 
-            <div className=gi
+            <div className='porgressBarsCntr'>
+                <div className='HTMLinfoCntr'>
+                    <div className='HTMLlogo-name'>
+                        <p className='htmlName'>HTML</p>
+                        <AiFillHtml5 className='htmlLogo'/>
+                    </div>
+
+                    <div className='progressBar HTML'><div className='HTMLfill'>70%</div></div>
+                </div>
+
+                <div className='CSSinfoCntr'>
+                    <div className='CSSlogo-name'>
+                        <p className='cssName'>CSS</p>
+                        <IoLogoCss3 className='cssLogo'/>
+                    </div>
+
+                    <div className='progressBar CSS'><div className='CSSfill'>60%</div></div>
+                </div>
+
+                <div className='JSinfoCntr'>
+                    <div className='JSlogo-name'>
+                        <p className='jsName'>Javascript</p>
+                        <SiJavascript className='jsLogo'/>
+                    </div>
+
+                    <div className='progressBar JS'><div className='JSfill'>65%</div></div>
+                </div>
+
+                <div className='ReactinfoCntr'>
+                    <div className='Reactlogo-name'>
+                        <p className='reactName'>React</p>
+                        <SiReact className='reactLogo'/>
+                    </div>
+
+                    <div className='progressBar React'><div className='Reactfill'>50%</div></div>
+                </div>
+
+                <div className='NodeinfoCntr'>
+                    <div className='Nodelogo-name'>
+                        <p className='nodeName'>Node</p>
+                        <SiNodedotjs className='nodeLogo'/>
+                    </div>
+
+                    <div className='progressBar Node'><div className='Nodefill'>20%</div></div>
                 </div>
             </div>
         </div>
