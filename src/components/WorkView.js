45348bd7739f0b33a978f29360d0b8fff10309aa
Rio0628 <mariomdomenech@gmail.com>
2022-01-06 23:12:46 -0500

    Created Comp
    - Created the main work view component which will harbor all of the different projects that will be shown.
    - This component itself will be able to be contracted and expanded to show all of the projects not just the preview few.

--- src/components/WorkView.js
+++ src/components/WorkView.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BsChevronCompactDown, BsChevronContract } from 'react-icons/bs'
 
 const WorkView = () => {
     return (
         <div className='workView' id='work'>
-            
+            <p className={'workViewHeading'}>Work - Projects</p>
+
+            <div className='projectsCntr'>
+                <div className='projectsPreviewCntr'>
+                
+                </div>
+
+                <div className='expandCntrBtn'><BsChevronCompactDown className='icon'/></div>
+
+                <div className='contractCntrBtn'><BsChevronContract className='icon'/></div>
+            </div>
         </div>
     );
 }
