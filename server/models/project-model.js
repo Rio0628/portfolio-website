a79d6e1a24610ccadd1607ca257ddd0aa5bc9d10
Rio0628 <mariomdomenech@gmail.com>
2022-01-20 11:28:37 -0500

    Altered the Project Model

--- server/models/project-model.js
+++ server/models/project-model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Project = new mongoose.Schema({
     name: { type: String, required: true },
     image: { type: String, required: true },
-    technologies: { type: String, required: true },
+    technologies: {type: [String], required: true },
     url: { type: String, required: true },
 }, { timestamps: true } );
 
