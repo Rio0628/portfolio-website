562f0ae010a2a7705563f611d6ba2f375a1ea426
Rio0628 <mariomdomenech@gmail.com>
2022-02-08 23:36:20 -0500

    Created Home View and Styled Basic Element

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import Typical from 'react-typical';
+import './HomeView.css';
 
 const HomeView = () => {
     return (
         <div className='homeView'>
-            Hello
+            {/* CANVAS OBJECT WILL GO HERE */}
+        
+            <p className='mainTxtBG'>Welcome</p>
+
+            <div className='welcomeTxt-DetailsCntr'>
+                <p className='welcomeTxt'>Hello I am Mario Domenech</p>
+                <div className='homeViewDetails'>
+                    <span className='detailsText'>
+                        <Typical 
+                            loop={Infinity}
+                            steps={[
+                                "Front-End Developer",
+                                2000,
+                                "React Developer",
+                                2000,
+                                "Angular Developer",
+                                2000,
+                                "Enthusiastic Dev",
+                                2000,
+                                "Full Stack Developer",
+                                2000,
+                            ]}
+                        />
+                    </span>
+                </div>
+            </div>
         </div>
     );
 }
