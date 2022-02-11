66f6c377a2d3dd57b54b2ca2ee6ea0b904a6ee89
Rio0628 <mariomdomenech@gmail.com>
2022-02-11 15:38:28 -0500

    Created Basic Canvas Text Sphere

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 // import * as TagCanvas from 'jquery-tagcanvas';
 
 const AboutView = (props) => {
@@ -6,34 +6,73 @@ const AboutView = (props) => {
     // Will trigger the animations in the about view once it is within the viewport 
     // const aboutViewActive = () => props.aboutViewVisible ? ' active' : '';
 
+    const skills = [
+        { href: "#git_", title: "Git" },
+        { href: "#json_", title: "JSON" },
+        { href: "#solidity_", title: "Solidity" },
+        { href: "#html_", title: "HTML" },
+        { href: "#react_", title: "React" },
+        { href: "#python_", title: "Python" },
+        { href: "#sql_", title: "SQL" },
+        { href: "#shopify_", title: "Shopify" },
+        { href: "#aws_", title: "AWS" },
+        { href: "#wordpress_", title: "WordPress" },
+        { href: "#npm_", title: "npm" },
+        { href: "#css_", title: "CSS" },
+        { href: "#jquery_", title: "jQuery" },
+        { href: "#js_", title: "JavaScript" },
+        { href: "#c++_", title: "C++" },
+        { href: "#java_", title: "Java" },
+        { href: "#php_", title: "PHP" },
+        { href: "#selenium_", title: "Selenium" },
+        { href: "#remix_", title: "Remix" },
+        { href: "#ganache_", title: "Ganache" },
+        { href: "#hubspot_", title: "HubSpot" },
+        { href: "#node.js_", title: "Node.js" },
+        { href: "#hiveos_", title: "HiveOS" },
+        { href: "#crypto_", title: "Crypto" },
+        { href: "#mining_", title: "Mining" },
+        { href: "#photoshop_", title: "Photoshop" },
+        { href: "#illustrator_", title: "Illustrator" },
+        { href: "#premier-pro_", title: "Premier Pro" },
+        { href: "#truffle_", title: "Truffle" },
+        { href: "#pyqt_", title: "PyQT" },
+        { href: "#hardhat_", title: "Hardhat" }
+    ];
+
     useEffect( () => {
-        console.log('mario');
-    
+      
+        
         const TagCanvas = window.TagCanvas;
         const tagCanvasOptions = {
            textColour: '#fff',
            outlineThickness: 0.5,
-           outlineColour: '#fff',
-           maxSpeed: 0.6,
+           outlineColour: '#FE0853',
+           maxSpeed: 0.1,
            freezeActive: true,
            shuffleTags: true,
            shape: 'sphere',
-           zoom: .8,
-           wheelZoom: false,
+           zoom: 1,
+           wheelZoom: true,
            noSelect: true,
-           textFont: null,
+           textFont: 'Titillium Web',
+        //    textWeight: 600,
+           textHeight: 12,
+           reverse: true,
            freezeDecel: true,
            fadeIn: 3000,
-           initial: [0.3, -.1],
+        //    initial: [0.3, -.1],
            depth: 1.1,
 
         };
         try {
-           TagCanvas.Start('canvas', 'tags', tagCanvasOptions);
+           TagCanvas.Start('canvas', 'allTags', tagCanvasOptions);
+            console.log(TagCanvas)
         } catch (e) {
             console.log('Canvas Error: ');
             console.log(e)
         }
+
 
     }, []);
 
@@ -67,16 +106,14 @@ const AboutView = (props) => {
             {/* <p className='skillsTitle'>Skills</p> */}
 
             <div className='skillCanvasCntr'>
-                <canvas className='skillCanvas' id='canvas' height='100' width='100'></canvas>
+                <canvas className='skillCanvas' id='canvas' style={{ width: '100%', height: '100%'}}></canvas>
             </div>
 
-            <div className='tags' id='tags'>
+            <div className='tags' id='allTags' style={{ display: 'none'}}>
                 <ul>
-                    <li>TEST</li>
-                    <li>TEST 1</li>
-                    <li>TEST 2</li>
-                    <li>TEST 3</li>
-                    <li>TEST 4</li>
+                    {skills.map(skill => (
+                        <li key={skill.title}><a href={skill.href}>{skill.title}</a></li>
+                    ))}
                 </ul>
             </div>
         </div>
