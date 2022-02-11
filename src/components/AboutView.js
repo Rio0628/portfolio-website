59e79cf8481f741db2c10ffe5366250180b55d3f
Rio0628 <mariomdomenech@gmail.com>
2022-02-11 14:59:25 -0500

    Installed TagCanvas.js Package

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+// import * as TagCanvas from 'jquery-tagcanvas';
 
 const AboutView = (props) => {
 
     // Will trigger the animations in the about view once it is within the viewport 
     // const aboutViewActive = () => props.aboutViewVisible ? ' active' : '';
 
+    useEffect( () => {
+        console.log('mario');
+    
+        const TagCanvas = window.TagCanvas;
+        const tagCanvasOptions = {
+           textColour: '#fff',
+           outlineThickness: 0.5,
+           outlineColour: '#fff',
+           maxSpeed: 0.6,
+           freezeActive: true,
+           shuffleTags: true,
+           shape: 'sphere',
+           zoom: .8,
+           wheelZoom: false,
+           noSelect: true,
+           textFont: null,
+           freezeDecel: true,
+           fadeIn: 3000,
+           initial: [0.3, -.1],
+           depth: 1.1,
+
+        };
+        try {
+           TagCanvas.Start('canvas', 'tags', tagCanvasOptions);
+        } catch (e) {
+            console.log('Canvas Error: ');
+            console.log(e)
+        }
+
+    }, []);
+
     return (
         <div className='aboutView' >
+
             <p className='mainTxtBG'>About</p>
 
             <p className='aboutMeTitle'>About Me</p>
@@ -31,7 +64,21 @@ const AboutView = (props) => {
                 </p>
             </div>
 
-            <p className='skillsTitle'>Skills</p>
+            {/* <p className='skillsTitle'>Skills</p> */}
+
+            <div className='skillCanvasCntr'>
+                <canvas className='skillCanvas' id='canvas' height='100' width='100'></canvas>
+            </div>
+
+            <div className='tags' id='tags'>
+                <ul>
+                    <li>TEST</li>
+                    <li>TEST 1</li>
+                    <li>TEST 2</li>
+                    <li>TEST 3</li>
+                    <li>TEST 4</li>
+                </ul>
+            </div>
         </div>
     );
 }
