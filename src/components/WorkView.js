5ef59dbe1d61f25082d2f303f8dd2f1de43f49b7
Rio0628 <mariomdomenech@gmail.com>
2022-02-14 10:55:09 -0500

    Deleted IndProjects comp from app

--- src/components/WorkView.js
+++ src/components/WorkView.js
@@ -1,37 +1,17 @@
 import React from 'react';
-import { BsChevronCompactDown, BsChevronContract } from 'react-icons/bs'
-import IndProject from './IndProject'; 
 
 const WorkView = (props) => {
-    let indProjectsCntr = [];
 
-    // For statement to create ind project components according the objects gathered from the database 
-    if (props.projects) {
-        for (let i = 0; i < props.projects.length; i++) {
-            indProjectsCntr.push( <IndProject project={props.projects[i]} key={'project' + i} />);
-        }
-    }
+    
 
     return (
-        <div className='workView' id='work' ref={props.setWorkRef}>
-            <p className={ props.workViewVisible ? 'workViewHeading active' : 'workViewHeading'}>Work - Projects</p>
+        <div className='workView'>
+          <p className='mainTxtBG'>Projects</p>
+          <div className='projectsCntr'>
+            <div className='indProject'>
 
-            <div className={ props.workViewVisible ? 'projectsCntr active' : 'projectsCntr'}>
-
-                <div className={ props.expandDropdown ? 'projectsPreviewCntr active' : 'projectsPreviewCntr'}>
-                    { indProjectsCntr }
-                </div>
-
-                {!props.expandDropdown ? 
-                    <div className='expandCntrBtn' onClick={ () => props.setExpandDropdown(true) }><BsChevronCompactDown className='icon'/></div>
-                : ''}
-
-                {props.expandDropdown ? 
-                    <div className='contractCntrBtn' onClick={ () => props.setExpandDropdown(false) }><BsChevronContract className='icon'/></div>
-                : ''}
-                
-                {/*  */}
             </div>
+          </div>
         </div>
     );
 }
