d02f691f14373117ca41dc1d8d6146b8d3bd550e
Rio0628 <mariomdomenech@gmail.com>
2022-02-14 23:21:39 -0500

    Created the rest of the projects placeholders that will be showcased in the website

--- src/components/WorkView.js
+++ src/components/WorkView.js
@@ -42,6 +42,171 @@ const WorkView = (props) => {
               </div>
             </div>
 
+            <div className='indProject'>
+              <p className='projectTitle'>Project</p>
+
+              <div className='projectPixCntr'>
+                <div className='projectPixShade'></div>
+                <img className='projectPix' alt='Project' />
+              </div>
+
+              <div className='projectInfoCntr'>
+                <p className='projectInfo'>
+                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
+                </p>
+
+                <div className='projectTechsCntr'>
+                  <p className='projectTech'>Tech 1</p>
+                  <p className='projectTech'>Tech 2</p>
+                  <p className='projectTech'>Tech 3</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 5</p>
+                  <p className='projectTech'>Tech 5</p>
+                </div>
+
+                <div className='projectBtnsCntr'>
+                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
+                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
+                </div>
+            
+              </div>
+            </div>
+
+            <div className='indProject'>
+              <p className='projectTitle'>Project</p>
+
+              <div className='projectPixCntr'>
+                <div className='projectPixShade'></div>
+                <img className='projectPix' alt='Project' />
+              </div>
+
+              <div className='projectInfoCntr'>
+                <p className='projectInfo'>
+                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
+                </p>
+
+                <div className='projectTechsCntr'>
+                  <p className='projectTech'>Tech 1</p>
+                  <p className='projectTech'>Tech 2</p>
+                  <p className='projectTech'>Tech 3</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 5</p>
+                  <p className='projectTech'>Tech 5</p>
+                </div>
+
+                <div className='projectBtnsCntr'>
+                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
+                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
+                </div>
+            
+              </div>
+            </div>
+
+            <div className='indProject'>
+              <p className='projectTitle'>Project</p>
+
+              <div className='projectPixCntr'>
+                <div className='projectPixShade'></div>
+                <img className='projectPix' alt='Project' />
+              </div>
+
+              <div className='projectInfoCntr'>
+                <p className='projectInfo'>
+                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
+                </p>
+
+                <div className='projectTechsCntr'>
+                  <p className='projectTech'>Tech 1</p>
+                  <p className='projectTech'>Tech 2</p>
+                  <p className='projectTech'>Tech 3</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 5</p>
+                  <p className='projectTech'>Tech 5</p>
+                </div>
+
+                <div className='projectBtnsCntr'>
+                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
+                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
+                </div>
+            
+              </div>
+            </div>
+
+            <div className='indProject'>
+              <p className='projectTitle'>Project</p>
+
+              <div className='projectPixCntr'>
+                <div className='projectPixShade'></div>
+                <img className='projectPix' alt='Project' />
+              </div>
+
+              <div className='projectInfoCntr'>
+                <p className='projectInfo'>
+                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
+                </p>
+
+                <div className='projectTechsCntr'>
+                  <p className='projectTech'>Tech 1</p>
+                  <p className='projectTech'>Tech 2</p>
+                  <p className='projectTech'>Tech 3</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 5</p>
+                  <p className='projectTech'>Tech 5</p>
+                </div>
+
+                <div className='projectBtnsCntr'>
+                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
+                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
+                </div>
+            
+              </div>
+            </div>
+
+            <div className='indProject'>
+              <p className='projectTitle'>Project</p>
+
+              <div className='projectPixCntr'>
+                <div className='projectPixShade'></div>
+                <img className='projectPix' alt='Project' />
+              </div>
+
+              <div className='projectInfoCntr'>
+                <p className='projectInfo'>
+                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
+                </p>
+
+                <div className='projectTechsCntr'>
+                  <p className='projectTech'>Tech 1</p>
+                  <p className='projectTech'>Tech 2</p>
+                  <p className='projectTech'>Tech 3</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 4</p>
+                  <p className='projectTech'>Tech 5</p>
+                  <p className='projectTech'>Tech 5</p>
+                </div>
+
+                <div className='projectBtnsCntr'>
+                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
+                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
+                </div>
+            
+              </div>
+            </div>
+
           </div>
         </div>
     );
