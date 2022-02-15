9b30e697f1606243ef10bb3d3176ac54ff1c1f9a
Rio0628 <mariomdomenech@gmail.com>
2022-02-15 15:41:29 -0500

    Gathered current projects within object and displayed them within comp. Added redirect funct

--- src/components/WorkView.js
+++ src/components/WorkView.js
@@ -3,218 +3,46 @@ import { BookFinder, TicTacToe, TodoListLogo, TimerLogo } from '../images/'
 
 const WorkView = (props) => {
 
-    
+    const projects = [
+      { name: 'Book Finder', image: 'book', description: "React application using google's API to fetch information on books and allow the user to make CRUD operations with said books. The user is also able to add comments on each book and classify it to a certain category", techs: ['React', 'Express', 'REST API', 
+      'API', 'MongoDB', 'Heroku'], seeLive: 'https://book-finder-mario-domenech.herokuapp.com/', seeCode: 'https://github.com/Rio0628/book-finder' },
 
+      { name: 'Tic Tac Toe', image: 'ticTacToe', description: "Simple React application that allows user to play against another user or against a CPU.Return to a past move within a game and choose which side to play as ", techs: ['React', 'Styled Components', 'Heroku'], seeLive: 'https://tic-tac-toe-mario-domenech.herokuapp.com/', seeCode: 'https://github.com/Rio0628/tic-tac-toe' },
+
+      { name: 'Todo List', image: 'todoList', description: "Full-Stack CRUD application that uses React, Express, and MongoDB. Utilizing a fully functional REST API and that connects to the front end side of application allowing an user to create, edit, and delete objects from the database", techs: ['React', 'Express', 'REST API', 'MONGO DB', 'Heroku'], seeLive: 'https://todo-list-mario-domenech.herokuapp.com/', seeCode: 'https://github.com/Rio0628/todo-list2.0' },
+
+      { name: 'Clock - Timer', image: 'clock', description: "Full-Stack CRUD application that uses React, Express, and MongoDB. Showcases time across different timezones, allows users to create sessions with different timer intervals, and save or delete sessions in database", techs: ['React', 'Express', 'REST API', 'MONGO DB', 'Heroku'], seeLive: 'https://clock-timer-mario-domenech.herokuapp.com/', seeCode: 'https://github.com/Rio0628/clock-timer' },
+    ];
     return (
         <div className='workView'>
           <p className='mainTxtBG'>Projects</p>
           <div className='projectsCntr'>
 
-            <div className='indProject'>
-              <p className='projectTitle'>Project</p>
-
-              <div className='projectPixCntr'>
-                <div className='projectPixShade'></div>
-                <div className='projectPix' />
-              </div>
-
-              <div className='projectInfoCntr'>
-                <p className='projectInfo'>
-                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
-                </p>
-
-                <div className='projectTechsCntr'>
-                  <p className='projectTech'>Tech 1</p>
-                  <p className='projectTech'>Tech 2</p>
-                  <p className='projectTech'>Tech 3</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 5</p>
-                  <p className='projectTech'>Tech 5</p>
+            { projects.map( project => 
+              <div className='indProject'>
+                <p className='projectTitle'>{project.name}</p>
+  
+                <div className='projectPixCntr'>
+                  <div className='projectPixShade'></div>
+                  <div className={'projectPix ' + project.image} />
                 </div>
-
-                <div className='projectBtnsCntr'>
-                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
-                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
+  
+                <div className='projectInfoCntr'>
+                  <p className='projectInfo'>{project.description}</p>
+  
+                  <div className='projectTechsCntr'>
+                    { project.techs.map( tech => <p className='projectTech'>{tech}</p>) }
+                  </div>
+  
+                  <div className='projectBtnsCntr'>
+                    <a className='porjectsBtn' id='seeAppLiveBtn' target='_blank' rel='noopener noreferrer' href={project.seeLive}>Live Demo</a>
+                    <a className='porjectsBtn' id='videCodeBtn' target='_blank' rel='noopener noreferrer' href={project.seeCode}>View Code</a>
+                  </div>
+              
                 </div>
-            
               </div>
-            </div>
-
-            <div className='indProject'>
-              <p className='projectTitle'>Project</p>
-
-              <div className='projectPixCntr'>
-                <div className='projectPixShade'></div>
-                <img className='projectPix' alt='Project' />
-              </div>
-
-              <div className='projectInfoCntr'>
-                <p className='projectInfo'>
-                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
-                </p>
-
-                <div className='projectTechsCntr'>
-                  <p className='projectTech'>Tech 1</p>
-                  <p className='projectTech'>Tech 2</p>
-                  <p className='projectTech'>Tech 3</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 5</p>
-                  <p className='projectTech'>Tech 5</p>
-                </div>
-
-                <div className='projectBtnsCntr'>
-                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
-                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
-                </div>
-            
-              </div>
-            </div>
-
-            <div className='indProject'>
-              <p className='projectTitle'>Project</p>
-
-              <div className='projectPixCntr'>
-                <div className='projectPixShade'></div>
-                <img className='projectPix' alt='Project' />
-              </div>
-
-              <div className='projectInfoCntr'>
-                <p className='projectInfo'>
-                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
-                </p>
-
-                <div className='projectTechsCntr'>
-                  <p className='projectTech'>Tech 1</p>
-                  <p className='projectTech'>Tech 2</p>
-                  <p className='projectTech'>Tech 3</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 5</p>
-                  <p className='projectTech'>Tech 5</p>
-                </div>
-
-                <div className='projectBtnsCntr'>
-                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
-                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
-                </div>
-            
-              </div>
-            </div>
-
-            <div className='indProject'>
-              <p className='projectTitle'>Project</p>
-
-              <div className='projectPixCntr'>
-                <div className='projectPixShade'></div>
-                <img className='projectPix' alt='Project' />
-              </div>
-
-              <div className='projectInfoCntr'>
-                <p className='projectInfo'>
-                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
-                </p>
-
-                <div className='projectTechsCntr'>
-                  <p className='projectTech'>Tech 1</p>
-                  <p className='projectTech'>Tech 2</p>
-                  <p className='projectTech'>Tech 3</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 5</p>
-                  <p className='projectTech'>Tech 5</p>
-                </div>
-
-                <div className='projectBtnsCntr'>
-                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
-                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
-                </div>
-            
-              </div>
-            </div>
-
-            <div className='indProject'>
-              <p className='projectTitle'>Project</p>
-
-              <div className='projectPixCntr'>
-                <div className='projectPixShade'></div>
-                <img className='projectPix' alt='Project' />
-              </div>
-
-              <div className='projectInfoCntr'>
-                <p className='projectInfo'>
-                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
-                </p>
-
-                <div className='projectTechsCntr'>
-                  <p className='projectTech'>Tech 1</p>
-                  <p className='projectTech'>Tech 2</p>
-                  <p className='projectTech'>Tech 3</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 5</p>
-                  <p className='projectTech'>Tech 5</p>
-                </div>
-
-                <div className='projectBtnsCntr'>
-                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
-                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
-                </div>
-            
-              </div>
-            </div>
-
-            <div className='indProject'>
-              <p className='projectTitle'>Project</p>
-
-              <div className='projectPixCntr'>
-                <div className='projectPixShade'></div>
-                <img className='projectPix' alt='Project' />
-              </div>
-
-              <div className='projectInfoCntr'>
-                <p className='projectInfo'>
-                  This is the description for the project. This project uses this one technology with this other technology and this other technology. you can use this really cool app
-                </p>
-
-                <div className='projectTechsCntr'>
-                  <p className='projectTech'>Tech 1</p>
-                  <p className='projectTech'>Tech 2</p>
-                  <p className='projectTech'>Tech 3</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 4</p>
-                  <p className='projectTech'>Tech 5</p>
-                  <p className='projectTech'>Tech 5</p>
-                </div>
-
-                <div className='projectBtnsCntr'>
-                  <p className='porjectsBtn' id='seeAppLiveBtn'>Live Demo</p>
-                  <p className='porjectsBtn' id='videCodeBtn'>View Code</p>
-                </div>
-            
-              </div>
-            </div>
-    
+            )}
           </div>
-
-            <p>TEST FOR TEST</p>
-            <img alt='testForImage' src={BookFinder}></img>
-            <img alt='testForImage' src={TimerLogo}></img>
-            <img alt='testForImage' src={TodoListLogo}></img>
-            <img alt='testForImage' src={TicTacToe}></img>
   
         </div>
     );
