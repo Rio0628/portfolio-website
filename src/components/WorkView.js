7bb58b05ab854da26d5a1401dbae655e3eaee96d
Rio0628 <mariomdomenech@gmail.com>
2022-04-02 14:49:23 -0400

    Updated projects view

--- src/components/WorkView.js
+++ src/components/WorkView.js
@@ -5,20 +5,22 @@ const WorkView = (props) => {
     const useOnScreen = props.useOnScreen;
     const options = props.options;
 
-    let prjct1RefBG = useRef(null), prjct2RefBG = useRef(null), prjct3RefBG = useRef(null), prjct4RefBG = useRef(null);
+    let prjct1RefBG = useRef(null), prjct2RefBG = useRef(null), prjct3RefBG = useRef(null), prjct4RefBG = useRef(null), prjct5RefBG = useRef(null);
 
-    let prjct1RefTXT = useRef(null), prjct2RefTXT = useRef(null), prjct3RefTXT = useRef(null), prjct4RefTXT = useRef(null);
+    let prjct1RefTXT = useRef(null), prjct2RefTXT = useRef(null), prjct3RefTXT = useRef(null), prjct4RefTXT = useRef(null), prjct5RefTXT = useRef(null);
     
-    let prjct1Anims = gsap.timeline({ paused: true }), prjct2Anims = gsap.timeline({ paused: true }), prjct3Anims = gsap.timeline({ paused: true }), prjct4Anims = gsap.timeline({ paused: true });
+    let prjct1Anims = gsap.timeline({ paused: true }), prjct2Anims = gsap.timeline({ paused: true }), prjct3Anims = gsap.timeline({ paused: true }), prjct4Anims = gsap.timeline({ paused: true }), prjct5Anims = gsap.timeline({ paused: true });
 
     const projects = [
       { name: 'WeatherMaps', image: 'weatherMaps', description: "Full Stack React application with Express rest API using MySQL database. Implements Mapbox API to render map and OpenWeather API to fetch weather data on different locations. Users will be able to save locations within database for later viewing.", techs: ['React', 'Express', 'REST API', 'GSAP', 'MySQL', 'APIs', 'Heroku'], seeLive: 'https://maps-weather-md.herokuapp.com/', seeCode: 'https://github.com/Rio0628/maps-weather',  refBG: prjct1RefBG, refTXT: prjct1RefTXT, anim: prjct1Anims },
 
-      { name: 'Book Finder', image: 'book', description: "React application using google's API to fetch information on books and allow the user to make CRUD operations with said books. The user is also able to add comments on each book and classify it to a certain category", techs: ['React', 'Express', 'REST API', 'API', 'MongoDB', 'Heroku'], seeLive: 'https://book-finder-mario-domenech.herokuapp.com/', seeCode: 'https://github.com/Rio0628/book-finder', refBG: prjct2RefBG, refTXT: prjct2RefTXT, anim: prjct2Anims },
+      { name: 'Book Finder', image: 'book', description: "React application using google's API to fetch information on books and allow the user to make CRUD operations with said books. The user is also able to add comments on each book and classify it to a certain category", techs: ['React', 'Express', 'GSAP', 'REST API', 'API', 'MySQL', 'Heroku'], seeLive: 'https://book-finder-mario-domenech.herokuapp.com/', seeCode: 'https://github.com/Rio0628/book-finder', refBG: prjct2RefBG, refTXT: prjct2RefTXT, anim: prjct2Anims },
 
-      { name: 'Todo List', image: 'todoList', description: "Full-Stack CRUD application that uses React, Express, and MongoDB. Utilizing a fully functional REST API and that connects to the front end side of application allowing an user to create, edit, and delete objects from the database", techs: ['React', 'Express', 'REST API', 'Mongo DB', 'Heroku'], seeLive: 'https://todo-list-mario-domenech.herokuapp.com/', seeCode: 'https://github.com/Rio0628/todo-list2.0',  refBG: prjct3RefBG, refTXT: prjct3RefTXT, anim: prjct3Anims },
+      { name: 'ExercisePedia', image: 'exercisePedia', description: "Fullstack React / Express application that utilizes MySQL database and Exercise DB REST API in order to retrieve information about different exercises and save them into different categories within the MySQL database. Utilizes CRUD functionality in order to allow an user to make changes of categories and exercises. ", techs: ['React', 'Express', 'REST API', 'Mongo DB', 'Heroku'], seeLive: 'https://exercise-pedia.herokuapp.com/', seeCode: 'https://github.com/Rio0628/exercise-pedia',  refBG: prjct3RefBG, refTXT: prjct3RefTXT, anim: prjct3Anims },
 
       { name: 'Clock - Timer', image: 'clock', description: "Full-Stack CRUD application that uses React, Express, and MongoDB. Showcases time across different timezones, allows users to create sessions with different timer intervals, and save or delete sessions in database", techs: ['React', 'Express', 'REST API', 'Mongo DB', 'Heroku'], seeLive: 'https://clock-timer-mario-domenech.herokuapp.com/', seeCode: 'https://github.com/Rio0628/clock-timer',  refBG: prjct4RefBG, refTXT: prjct4RefTXT, anim: prjct4Anims },
+
+      { name: 'Todo List', image: 'todoList', description: "Full-Stack CRUD application that uses React, Express, and MongoDB. Utilizing a fully functional REST API and that connects to the front end side of application allowing an user to create, edit, and delete objects from the database", techs: ['React', 'Express', 'REST API', 'Mongo DB', 'Heroku'], seeLive: 'https://todo-list-mario-domenech.herokuapp.com/', seeCode: 'https://github.com/Rio0628/todo-list2.0',  refBG: prjct5RefBG, refTXT: prjct5RefTXT, anim: prjct5Anims },
     ];
     
     let titleRef = useRef(null);
@@ -53,8 +55,11 @@ const WorkView = (props) => {
 
       prjct4Anims.to( prjct4RefBG.current, { opacity: 1, duration: .1 });
       prjct4Anims.to( prjct4RefTXT.current, { x: 0, duration: .1 });
+
+      prjct5Anims.to( prjct5RefBG.current, { opacity: 1, duration: .1 });
+      prjct5Anims.to( prjct5RefTXT.current, { x: 0, duration: .1 });
       
-    }, [prjct1Anims, prjct2Anims, prjct3Anims, prjct4Anims, popupAnims]);
+    }, [prjct1Anims, prjct2Anims, prjct3Anims, prjct4Anims, prjct5Anims, popupAnims]);
   
     if (conditional) { popupAnims.play(); }
 
